642bcdf209227f93f89e32895776f007f5ec420c
Robert Putnam <thechucklingatom@gmail.com>
2015-12-31 10:43:56 -0700

    Added manifest and created fragment layout file

---
+++ main/java/com/thechucklingatom/mtgcollectionmanager/CardAndSetLisViewFragment.java
@@ -0,0 +1,11 @@
+package com.thechucklingatom.mtgcollectionmanager;
+
+import android.app.Fragment;
+
+/**
+ * Created by thechucklingatom on 12/30/2015.
+ */
+public class CardAndSetLisViewFragment extends Fragment {
+
+
+}
