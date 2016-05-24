8c30d69c705951cf563181ba60193284e4d219ac
Robert Putnam <thechucklingatom@gmail.com>
2016-05-23 20:24:24 -0600

    Added image grabbing, so the card will pull the image from wizards website. I will be working on settings option that will choose the image or no image based on that setting. After that I will be working on searchability of the lists when on the set side or the card side.

---
+++ main/java/com/thechucklingatom/mtgcollectionmanager/CardImageFetcher.java
@@ -0,0 +1,57 @@
+package com.thechucklingatom.mtgcollectionmanager;
+
+import android.app.Activity;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Gets the image for a magic card based on its multiverse Id and this link http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=@multiverseId&type=card
+ *
+ * @author thechucklingatom
+ *
+ */
+public class CardImageFetcher extends AsyncTask<Card, Void, Drawable>{
+
+    Activity fetchingActivity;
+
+    public CardImageFetcher(Activity activity){
+        fetchingActivity = activity;
+    }
+
+    @Override
+    protected Drawable doInBackground(Card... cards) {
+        try{
+            String url = "http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=" +
+                    cards[0].getMultiverseid() + "&type=card";
+            InputStream inputStream = (InputStream) new URL(url).getContent();
+            Log.i("doInBackground: ", "inputStream gotten");
+            return Drawable.createFromStream(inputStream, "src name");
+        } catch (MalformedURLException e) {
+            Log.e("fetchImage: ", "Error with URL", e.fillInStackTrace());
+            e.printStackTrace();
+        } catch (IOException e) {
+            Log.e("fetchImage: ", "Error reading image", e.fillInStackTrace());
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    protected void onPostExecute(Drawable cardImage){
+        Log.i("onPostExecute: ", "post execute");
+        if(cardImage != null){
+            Log.i("onPostExecute: ", "non null image");
+            ImageView imageCard = (ImageView)fetchingActivity.findViewById(R.id.imageView);
+            imageCard.setImageDrawable(cardImage);
+            imageCard.setScaleType(ImageView.ScaleType.FIT_XY);
+            imageCard.setVisibility(View.VISIBLE);
+        }
+    }
+}
--- main/java/com/thechucklingatom/mtgcollectionmanager/CardView.java
+++ main/java/com/thechucklingatom/mtgcollectionmanager/CardView.java
@@ -1,15 +1,22 @@
 package com.thechucklingatom.mtgcollectionmanager;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.concurrent.ExecutionException;
+
 public class CardView extends AppCompatActivity {
+
+    CardImageFetcher imageFetcher = new CardImageFetcher(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,6 +65,9 @@ public class CardView extends AppCompatActivity {
         }else{
             textPowerAndToughness.setText("");
         }
+
+        imageFetcher.execute(displayedCard);
+
     }
 
 }
